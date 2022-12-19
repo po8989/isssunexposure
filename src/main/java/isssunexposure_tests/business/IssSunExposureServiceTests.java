680a6d02b26dc64ed49b650acc49caed15726f23
Pierre-Olivier Blondiau <poblondiau@gmail.com>
2022-12-19 18:05:47 +0100

    Add unit tests to test IssSunExposureService methods

--- src/main/java/isssunexposure_tests/business/IssSunExposureServiceTests.java
+++ src/main/java/isssunexposure_tests/business/IssSunExposureServiceTests.java
@@ -3,30 +3,13 @@ package isssunexposure_tests.business;
 import io.quarkus.test.junit.QuarkusTest;
 import isssunexposure.business.IIssSunExposureService;
 import isssunexposure.business.IssSunExposureService;
-import isssunexposure.helpers.SatelliteParserHelper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
 public class IssSunExposureServiceTests {
 
     private IIssSunExposureService issSunExposureService = new IssSunExposureService();
-
-    @Test
-    public void IssSunExposureService_GetSunExposureWindowHistory_VerifySatelliteRetrieved() throws Exception {
-        // Arrange
-        var satelliteId = 25544;
-
-        // Act
-        var iss = issSunExposureService.GetSunExposureWindowHistory(satelliteId);
-        var satellite = SatelliteParserHelper.ParseSatelliteAsObject(iss);
-
-        // Assert
-        assertEquals(satelliteId, satellite.id);
-        assertEquals("iss", satellite.name);
-    }
 
     @Test
     public void IssSunExposureService_GetSunExposureWindowHistory_VerifySatelliteNotExisting_throwsException() {
@@ -42,23 +25,5 @@ public class IssSunExposureServiceTests {
         }
         catch (Exception e){
         }
-    }
-
-    @Test
-    public void IssSunExposureService_GetSunExposureWindowHistory_VerifySatelliteAddedToCache() {
-        // Arrange
-
-        // Act
-
-        // Assert
-    }
-
-    @Test
-    public void IssSunExposureService_GetSunExposureWindowHistory_WithAPIDown_GetSatelliteFromCache() {
-        // Arrange
-
-        // Act
-
-        // Assert
     }
 }
