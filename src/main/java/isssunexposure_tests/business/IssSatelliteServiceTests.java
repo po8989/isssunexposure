8ab6ec9d2a53cd0fe6401cd6ef33b2f11f6a7cbe
Pierre-Olivier Blondiau <poblondiau@gmail.com>
2022-12-19 19:10:04 +0100

    Add unit tests to test IssSatelliteService methods

---
+++ src/main/java/isssunexposure_tests/business/IssSatelliteServiceTests.java
@@ -0,0 +1,92 @@
+package isssunexposure_tests.business;
+
+import io.quarkus.test.junit.QuarkusTest;
+import isssunexposure.business.IIssSatelliteService;
+import isssunexposure.business.IssSatelliteService;
+import isssunexposure.models.Satellite;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@QuarkusTest
+public class IssSatelliteServiceTests {
+
+    private static IIssSatelliteService issSatelliteService;
+
+    @BeforeAll
+    public static void TestInitialise()
+    {
+        issSatelliteService = new IssSatelliteService();
+    }
+
+    @Test
+    public void IssSunExposureService_GetSatelliteById_VerifySatelliteRetrieved() throws Exception {
+        // Arrange
+        var satelliteId = 25544;
+
+        // Act
+        var iss = issSatelliteService.GetSatelliteById(satelliteId);
+
+        // Assert
+        assertEquals(satelliteId, iss.id);
+        assertEquals("iss", iss.name);
+    }
+
+    @Test
+    public void IssSunExposureService_GetSunExposureWindowHistory_VerifySatelliteNotExisting_throwsException() {
+        // Arrange
+        var satelliteId = 999999;
+        Satellite iss = null;
+        // Act
+        try {
+            iss = issSatelliteService.GetSatelliteById(satelliteId);
+        }
+        catch (Exception e){
+        }
+
+        // Assert
+        assertNull(iss);
+    }
+
+    @Test
+    public void IssSunExposureService_GetSatelliteById_VerifySatelliteAddedToCache() {
+        // TODO Find a way to expose private methods
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void IssSunExposureService_GetSatelliteById_WithAPIDown_GetSatelliteFromCache() {
+        // TODO Find a way to expose private methods
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void IssSunExposureService_GetSatelliteById_WithACallDoneLessThan20SecondsAgo_CallISSAPI() {
+        // TODO Find a way to expose private methods
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void IssSunExposureService_GetSatelliteById_WithACallDoneMoreThan20SecondsAgo_DoNotCallISSAPI() {
+        // TODO Find a way to expose private methods
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
--- src/main/java/isssunexposure_tests/business/IssSunExposureServiceTests.java
+++ src/main/java/isssunexposure_tests/business/IssSunExposureServiceTests.java
@@ -3,27 +3,26 @@ package isssunexposure_tests.business;
 import io.quarkus.test.junit.QuarkusTest;
 import isssunexposure.business.IIssSunExposureService;
 import isssunexposure.business.IssSunExposureService;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 public class IssSunExposureServiceTests {
 
-    private IIssSunExposureService issSunExposureService = new IssSunExposureService();
+    private static IIssSunExposureService issSunExposureService;
+
+    @BeforeAll
+    public static void TestInitialise(){
+        issSunExposureService = new IssSunExposureService();
+    }
 
     @Test
-    public void IssSunExposureService_GetSunExposureWindowHistory_VerifySatelliteNotExisting_throwsException() {
+    public void IssSunExposureService_GetSunExposureWindowHistory_VerifySatelliteNotExisting_throwsException() throws Exception {
+        // TODO Find a way to Mock dependencies
         // Arrange
-        var satelliteId = 999999;
 
         // Act
-        try {
-            var iss = issSunExposureService.GetSunExposureWindowHistory(satelliteId);
 
-            // Assert
-            Assertions.assertEquals("No satellite found", iss);
-        }
-        catch (Exception e){
-        }
+        // Assert
     }
 }
